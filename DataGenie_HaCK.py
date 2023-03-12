9ae7bd631bb4ef9f4c169c883536e3530cff0c56
Artemis1702 <tejassrinivas2003@gmail.com>
2023-03-13 00:20:11 +0530

    final changes

--- DataGenie_HaCK.py
+++ DataGenie_HaCK.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[274]:
+# In[61]:
 
 
 import pandas as pd
@@ -14,7 +14,7 @@ from sklearn.metrics import mean_absolute_percentage_error
 from datetime import datetime, timedelta
 
 
-# In[228]:
+# In[62]:
 
 
 # Calculate MAPE values
@@ -22,30 +22,29 @@ def calculate_mape(actual, predicted):
     return np.mean(np.abs((actual - predicted) / actual)) * 100
 
 
-# In[229]:
+# In[63]:
 
 
 # Load the data
-data = pd.read_csv('C:\\Users\\tejas\\Desktop\\sample_1 daily.csv', parse_dates=['point_timestamp'], index_col='point_timestamp')
+data1 = pd.read_csv('C:\\Users\\tejas\\Desktop\\sample_1 daily.csv', parse_dates=['point_timestamp'], index_col='point_timestamp')
 
 # Set the frequency to daily
 
-data.index = pd.date_range(start=data.index.min(), end=data.index.max(), freq='D')
+data1.index = pd.date_range(start=data1.index.min(), end=data1.index.max(), freq='D')
+
+# Remove null values
+data1 = data1.dropna()
 
 
-data = data.drop('Unnamed: 0', axis = 1)
+data1 = data1.drop('Unnamed: 0', axis = 1)
 
 
-train_data = data.iloc[:int(len(data)*0.8)] #First 80 % is train data
-
-
-# In[230]:
+# In[64]:
 
 
 # Arima 
-def ARIM(start_date, end_date, data):
-    # last 20% of the data as testing data
-    test_data = data.iloc[int(len(data) * 0.8):]
+def ARIM(start_date, end_date, data, train_data = data1.iloc[:int(len(data)*0.8)]):
+    test_data = data.iloc[int(len(data) * 0.8):] # last 20% of the data as testing data
 
     # Fit the model
     arima_model = sm.tsa.arima.ARIMA(train_data['point_value'], order=(1,1,1)) # using an autoregressive term of order 1, a differencing term of order 1, and a moving average term of order 1
@@ -62,13 +61,12 @@ def ARIM(start_date, end_date, data):
 #     print('MAPE:', arima_mape)
 
 
-# In[231]:
+# In[65]:
 
 
 # SARIMA
-def SARIMA(data):
-    # last 20% of the data as testing data
-    test_data = data.iloc[int(len(data) * 0.8):]
+def SARIMA(data, train_data = data1.iloc[:int(len(data)*0.8)]):
+    test_data = data.iloc[int(len(data) * 0.8):] # last 20% of the data as testing data
 
     # Fit the SARIMA model
     sarima_model = sm.tsa.statespace.SARIMAX(train_data, order=(1, 1, 1), seasonal_order=(0, 0, 0, 7)) # 7 indicates that there is a daily seasonality in the data
@@ -83,13 +81,12 @@ def SARIMA(data):
 #     print('MAPE:', sarima_mape)
 
 
-# In[232]:
+# In[66]:
 
 
 # XGBoost
-def XGBoost(data):
-    # last 20% of the data as testing data
-    test_data = data[int(len(data) * 0.8):]
+def XGBoost(data, train_data = data1.iloc[:int(len(data)*0.8)]):
+    test_data = data[int(len(data) * 0.8):] # last 20% of the data as testing data
 
     # Define the features and target variable
     features = ['point_value']
@@ -108,13 +105,12 @@ def XGBoost(data):
 #     print('MAPE:', xgb_mape)
 
 
-# In[233]:
+# In[67]:
 
 
 # ETS
-def ETS(data):
-    # last 20% of the data as testing data
-    test_data = data[int(len(data) * 0.8):]
+def ETS(data, train_data = data1.iloc[:int(len(data)*0.8)]):
+    test_data = data[int(len(data) * 0.8):] # last 20% of the data as testing data
 
     # Create and fit the ETS model
     ets_model = ETSModel(train_data['point_value'].astype(float), error='add', trend='add', seasonal='add', seasonal_periods=7)
@@ -130,26 +126,84 @@ def ETS(data):
 #     print('MAPE:', ets_mape)
 
 
-# In[234]:
+# In[83]:
 
 
-# Selection Algorithm
-# I am running all the above functions of the time series models and choosing least mape, the above functions are the 
-# used in the selection algorithm
-def selection(start_date, end_date, data):
-    # Choose the model with the lowest MAPE
-    arima_mape = ARIM(start_date, end_date, data)
-    sarima_mape = SARIMA(data)
-    xgb_mape = XGBoost(data)
-    ets_mape = ETS(data)
-    mape_values = {"ARIMA": arima_mape, "SARIMA": sarima_mape, "XGBoost": xgb_mape, "ETS": ets_mape}
-    best_model = min(mape_values, key=mape_values.get)
+# Load the data which we will use to train the model with
+data2 = pd.read_csv('C:\\Users\\tejas\\Desktop\\sample_2 daily.csv', parse_dates=['point_timestamp'], index_col='point_timestamp')
+data3 = pd.read_csv('C:\\Users\\tejas\\Desktop\\sample_3 daily.csv', parse_dates=['point_timestamp'], index_col='point_timestamp')
+data4 = pd.read_csv('C:\\Users\\tejas\\Desktop\\sample_4 daily.csv', parse_dates=['point_timestamp'], index_col='point_timestamp')
+data5 = pd.read_csv('C:\\Users\\tejas\\Desktop\\sample_5 daily.csv', parse_dates=['point_timestamp'], index_col='point_timestamp')
+
+# Set their frequencies to Daily
+data2.index = pd.date_range(start=data2.index.min(), end=data2.index.max(), freq='D')
+data3.index = pd.date_range(start=data3.index.min(), end=data3.index.max(), freq='D')
+data4.index = pd.date_range(start=data4.index.min(), end=data4.index.max(), freq='D')
+data5.index = pd.date_range(start=data5.index.min(), end=data5.index.max(), freq='D')
+
+# Remove null values
+data2 = data2.dropna()
+data3 = data3.dropna()
+data4 = data4.dropna()
+data5 = data5.dropna()
+
+data2 = data2.drop('Unnamed: 0', axis = 1)
+data3 = data3.drop('Unnamed: 0', axis = 1)
+data4 = data4.drop('Unnamed: 0', axis = 1)
+data5 = data5.drop('Unnamed: 0', axis = 1)
+data5
+
+
+# In[107]:
+
+
+# Selection algorithm
+# I am running all the above Time Series models for multiple datasets and storing the Least MAPE values, the model which 
+# gives the least MAPE Values most number of times will be stored and then the new model used to train will also be added 
+# to the training data
+train_data2 = data2.iloc[:int(len(data2)*0.8)]
+train_data3 = data3.iloc[:int(len(data3)*0.8)]
+train_data4 = data4.iloc[:int(len(data4)*0.8)]
+train_data5 = data5.iloc[:int(len(data5)*0.8)]
+best_model_count = {"ARIMA": 0, "SARIMA": 0, "XGBoost": 0, "ETS": 0}
+train = [train_data2, train_data3, train_data4, train_data5]
+dat = [data2, data3, data4, data5]
+def select(data):   
+    for i in range(len(train)):
+        # Choose the model with the lowest MAPE
+        if i == 0:
+            arima_mape = ARIM('2021-10-16','2023-02-01', dat[i], train[i])
+        elif i == 1:
+            arima_mape = ARIM('2021-01-02','2021-09-19', dat[i], train[i])
+        elif i == 2:
+            arima_mape = ARIM('2022-02-21','2023-01-27',dat[i], train[i])
+        elif i == 3:
+            arima_mape = ARIM('2021-09-02','2023-01-18', dat[i], train[i])
+        sarima_mape = SARIMA(dat[i], train[i])
+        xgb_mape = XGBoost(dat[i], train[i])
+        ets_mape = ETS(dat[i], train[i])
+        mape_values = {"ARIMA": arima_mape, "SARIMA": sarima_mape, "XGBoost": xgb_mape, "ETS": ets_mape}
+        best_model = min(mape_values, key=mape_values.get)
+        best_model_count[best_model] += 1
+    if data not in dat:
+        dat.append(data)
+        train.append(data.iloc[:int(len(data)*0.8)])
+    
+
+
+# In[108]:
+
+
+# algorithm to use selection algorithm to check what model to use
+def select(start_date, end_date, data):
+    select(data)
+    best_model = min(best_model_count, key=mape_values.get)
     return best_model
 
 # print("The best model is: ", best_model)
 
 
-# In[239]:
+# In[109]:
 
 
 # Predict for test data using Best model and plot
@@ -219,7 +273,7 @@ def prediction(start_date, end_date, best_model, data) :
    return mape
 
 
-# In[253]:
+# In[110]:
 
 
 # A function which will get the response body for the fastapi
@@ -283,11 +337,12 @@ def predict1(start_date, end_date, best_model, data):
     return mape, pred 
 
 
-# In[277]:
+# In[111]:
 
 
 # This is the connecter function which gives me all the desired outputs needed for FAST API
 def connect(start_date, end_date, period):
+#     Check whether the data has periods to predict further
     if period > 0:
         end_date = datetime.strptime(end_date, '%Y-%m-%d')
         end_date = end_date + timedelta(days=period)
@@ -307,18 +362,18 @@ def connect(start_date, end_date, period):
 #     print(ind)
 
 
-# In[279]:
+# In[112]:
 
 
-# connect('2021-07-24', '2021-07-26',0)
+connect('2021-07-24', '2021-07-26',0)
 
 
-# In[257]:
+# In[113]:
 
 
-# b = selection('2019-07-17','2021-07-27',data)
-# ma = prediction('2019-07-17', '2021-07-27', b, data)
-# print(b,ma)
+b = selection('2019-07-17','2021-07-27',data)
+ma = prediction('2019-07-17', '2021-07-27', b, data)
+print(b,ma)
 
 
 # In[ ]:
